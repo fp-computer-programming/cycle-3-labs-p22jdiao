4eef672a7695ea28ba6223d9f32226a5c0977a0c
p22jdiao <p22jdiao@fairfieldprep.org>
2021-09-28 10:06:48 -0400

    lab-2.1 and lab-2.2

---
+++ lab_2-1.py
@@ -0,0 +1,6 @@
+# Author: JD 09/28/2021
+
+num = int(input("Please enter an integer: "))
+
+if num % 2 == 0:
+    print(num, "is an even number")
---
+++ lab_2-2.py
@@ -0,0 +1,8 @@
+# Author: JD 09/28/2021
+
+num = int(input("Please enter an integer: "))
+
+if num % 2 == 0:
+    print(num, "is an even number")
+else:
+    print(num, "is an odd number")
