f4cc32655e90d03c92c02b76e9c8a50e94f349eb
p22jdiao <p22jdiao@fairfieldprep.org>
2021-10-04 09:56:29 -0400

    5-1

---
+++ lab_5-1.py
@@ -0,0 +1,16 @@
+# Author: JD 10/04/2021
+
+# int(a)
+# NameError
+
+# int("a")
+# ValueError
+
+# "a" + 2
+# TypeError
+
+# import date
+# ModuleNotFoundError
+
+# print("I am a happy camper!)
+# SyntaxError
